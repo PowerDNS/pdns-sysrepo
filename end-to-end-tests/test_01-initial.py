1105a99aa4a1099288b1a02c3f7ee52676224741
Pieter Lexis <pieter.lexis@powerdns.com>
2019-12-09 14:58:00 +0100

    TESTS: Add some more end-to-end tests

--- end-to-end-tests/test_01-initial.py
+++ end-to-end-tests/test_01-initial.py
@@ -51,42 +51,44 @@ class PdnsSysrepoBasicTest(unittest.TestCase):
                              '--edit={}'.format(fp.name),
                              '-m', 'pdns-server'])
 
-    def testImported(self):
-        self.importConfig(
-            {
-                "pdns-server:pdns-server": {
-                    "backend": [
-                        {
-                            "name": "backend1",
-                            "backendtype": "lmdb",
-                            "filename": "{}/pdns.lmdb".format(self.dbdir)
-                        }
-                    ],
-                    "listen-addresses": [
-                        {
-                            "name": "main IP",
-                            "ip-address": "127.0.0.1",
-                            "port": "5300"
-                        }
-                    ],
-                    "webserver": {
-                        "port": self.webserver_port,
-                        "api-key": self.api_key
-                    },
-                    "master": False,
-                    "slave": True
-                }
-            }
-        )
+    @classmethod
+    def getRunningDS(cls) -> dict:
         output = subprocess.run(['sysrepocfg', '-X', '--format=json',
                                  '--module=pdns-server',
                                  '--datastore=running'],
                                 stdout=subprocess.PIPE,
                                 stderr=subprocess.PIPE)
         print(output)
-        data = json.loads(output.stdout)
+        return json.loads(output.stdout)
+
+    @classmethod
+    def getOperationalDS(cls) -> dict:
+        output = subprocess.run(['sysrepocfg', '-X', '--format=json',
+                                 '--module=pdns-server',
+                                 '--datastore=operational'],
+                                stdout=subprocess.PIPE,
+                                stderr=subprocess.PIPE)
+        print(output)
+        return json.loads(output.stdout)
+
+    def testImported(self):
+        self.importConfig(self.default_config)
+        data = self.getRunningDS()
         self.assertFalse(data["pdns-server:pdns-server"]["master"])
         self.assertTrue(data["pdns-server:pdns-server"]["slave"])
+        self.assertEqual(data["pdns-server:pdns-server"]["webserver"]["api-key"], self.api_key)
+        self.assertEqual(data["pdns-server:pdns-server"]["webserver"]["port"], self.webserver_port)
+        self.assertEqual(len(data["pdns-server:pdns-server"]["listen-addresses"]), 1)
+
+    def testUpdateConfig(self):
+        self.importConfig(self.default_config)
+        self.editConfig({
+            "pdns-server:pdns-server": {
+                "slave": False
+            }
+        })
+        data = self.getRunningDS()
+        self.assertFalse(data["pdns-server:pdns-server"]["slave"])
 
     def testAddZone(self):
         self.importConfig(self.default_config)
@@ -102,13 +104,40 @@ class PdnsSysrepoBasicTest(unittest.TestCase):
             ]
         })
 
-        output = subprocess.run(['sysrepocfg', '-X', '--format=json',
-                                 '--module=pdns-server',
-                                 '--datastore=operational'],
-                                stdout=subprocess.PIPE,
-                                stderr=subprocess.PIPE)
-        print(output)
-        data = json.loads(output.stdout)
+        data = self.getOperationalDS()
+        self.assertEqual(len(data["pdns-server:zones-state"]["zones"]), 1)
+        self.assertEqual(
+            data["pdns-server:zones-state"]["zones"][0]["name"], zonename)
+        self.assertEqual(
+            data["pdns-server:zones-state"]["zones"][0]["zonetype"],
+            zonetype)
+
+    def testChangeZoneType(self):
+        self.importConfig(self.default_config)
+        zonename = "edit.example.com."
+        zonetype = "native"
+        self.editConfig({
+            "pdns-server:zones": [
+                {
+                    "name": zonename,
+                    "class": "IN",
+                    "zonetype": zonetype
+                }
+            ]
+        })
+
+        zonetype = "master"
+        self.editConfig({
+            "pdns-server:zones": [
+                {
+                    "name": zonename,
+                    "class": "IN",
+                    "zonetype": zonetype
+                }
+            ]
+        })
+
+        data = self.getOperationalDS()
         self.assertEqual(len(data["pdns-server:zones-state"]["zones"]), 1)
         self.assertEqual(
             data["pdns-server:zones-state"]["zones"][0]["name"], zonename)
