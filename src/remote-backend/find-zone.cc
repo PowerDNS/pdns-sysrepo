PowerDNS/pdns-sysrepo

commit e5ba136294b1f055705ad1f93f9a277149a76860

CODE: Fix after comments

src/remote-backend/find-zone.cc
@@ -15,35 +15,20 @@
  */
 
 #include <boost/algorithm/string/join.hpp>
+#include <boost/algorithm/string/split.hpp>
+#include <boost/algorithm/string/classification.hpp>
 
 #include "remote-backend.hh"
 
 namespace pdns_sysrepo::remote_backend {
 
-using std::vector;
-
-vector<string> split(const string& str, const string& delim) {
-  vector<string> tokens;
-  size_t prev = 0, pos = 0;
-  do {
-    pos = str.find(delim, prev);
-    if (pos == string::npos)
-      pos = str.length();
-    string token = str.substr(prev, pos - prev);
-    if (!token.empty())
-      tokens.push_back(token);
-    prev = pos + delim.length();
-  } while (pos < str.length() && prev < str.length());
-  return tokens;
-}
-
 string RemoteBackend::findBestZone(const string& qname) {
   // I miss dnsname.cc :(
-  auto labels = split(qname, ".");
+  std::vector<std::string> labels;
+  boost::split(labels, qname, boost::is_any_of("."));
   string ret = qname;
   auto session = getSession();
 
-  // haha, no root support
   while (!labels.empty()) {
     try {
       string xpath = fmt::format("/pdns-server:zones/zones[name='{}']", ret);

src/remote-backend/httpfunc.cc
@@ -45,6 +45,7 @@ void RemoteBackend::notFound(const Rest::Request &request, Http::ResponseWriter 
 std::string RemoteBackend::urlDecode(std::string& eString) {
   // from http://www.cplusplus.com/forum/general/94849/
   std::string ret;
+  ret.reserve(eString.length());
   char ch;
   size_t i;
   unsigned int j;

commit 6f3eb0f3cdabb691c11dd8cc378df2c104976a80

CODE: Fix 'double dot' issue in find-zone
Introduced in e5ba136294b1f055705ad1f93f9a277149a76860

src/remote-backend/find-zone.cc
@@ -44,7 +44,10 @@ string RemoteBackend::findBestZone(const string& qname) {
       // Do nothing
     }
     labels.erase(labels.begin());
-    ret = boost::algorithm::join(labels, ".") + ".";
+    ret = boost::algorithm::join(labels, ".");
+    if (ret.empty()) {
+      ret = ".";
+    }
   }
   throw std::out_of_range(fmt::format("No zone found for qname {}", qname));
 }